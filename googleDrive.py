e1b6acefe7cca5120338b64bcd995b4f6eb06a53
0uz <duymaz_14@hotmail.com>
2021-03-21 14:38:59 +0300

    last

---
+++ googleDrive.py
@@ -0,0 +1,76 @@
+import pickle
+import os
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+import datetime
+from datetime import datetime
+import time
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    print(SCOPES)
+
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    # print(pickle_file)
+
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, 'service created successfully')
+        return service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
+        return None
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return dt
+
+def uploadFile():
+    API_NAME = 'drive'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    service = Create_Service('credentials.json',API_NAME,API_VERSION,SCOPES)
+    folderID = '19QrYgTBw3S3s2Qypqb6WaUtfDagA0QLY'
+    mime_type = 'application/x-sqlite3'
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    file_name = dt_string + '.db'
+
+    file_metadata = {
+        'name' : file_name,
+        'parents': [folderID],
+    }
+
+    upload = MediaFileUpload('test.db',mimetype=mime_type)
+
+    service.files().create(body=file_metadata,media_body=upload,fields='id').execute()
+
+def upload():
+    while True:
+        print("Backup Alindi")
+        uploadFile()
+        time.sleep(43200)
--- setup.py
+++ setup.py
@@ -7,16 +7,25 @@ import telegram
 import buyer
 import seller
 import stop
+import googleDrive
 
 bot = telebot.TeleBot("1628197070:AAFLvfUgbwO8qnY4YkQJ8yLHLoube-51GKc", parse_mode="MarkdownV2")
 
 if __name__ == '__main__':
-    buy = Thread(target=buyer.buyer)
-    sell = Thread(target=seller.seller)
-    stops = Thread(target=stop.stopTracker)
-    tele = Thread(target=telegram.pool)
+    buy = Process(target=buyer.buyer)
+    sell = Process(target=seller.seller)
+    stops = Process(target=stop.stopTracker)
+    tele = Process(target=telegram.pool)
+    drive = Process(target=googleDrive.upload)
 
     buy.start()
     sell.start()
     stops.start()
     tele.start()
+    drive.start()
+
+    buy.join()
+    sell.join()
+    stops.join()
+    tele.join()
+    drive.join()
